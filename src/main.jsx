35802826ffb37ba6310dea33d9b8bda145a39ee7
Bohdan Babych <babychbohdan0@gmail.com>
2024-04-01 20:47:36 +0200

    1 task update

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// import App from './App.jsx'
-// import './index.css'
+import App from './components/App';
+import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-const MyComponent = () => {
-  return <p>my first component</p>;
-};
-const MyComponent2 = () => {
-  return <div>hello world</div>;
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
