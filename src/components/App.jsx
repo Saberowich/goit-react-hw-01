e37b1eee6f0b1800edd467a1da0b01174472b964
Bohdan Babych <babychbohdan0@gmail.com>
2024-04-01 20:41:19 +0200

    1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,25 @@
 
+import { Profile } from "./Profile/Profile.jsx";
+import userData from "./userData.json";
+
+
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      
+    </>
+  );
+};
+
+
+export default App;
 
